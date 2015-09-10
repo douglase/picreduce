9d29046b8b54819c57384f00b8c635bd5dcabea0
douglase <douglase@bu.edu>
2015-09-10 09:19:48 -0400

    relative import and passing more keywords to max_cen_phot (and thus fwcentroid())

--- picreduce/plotting/convenient_plot.py
+++ picreduce/plotting/convenient_plot.py
@@ -3,7 +3,7 @@ from matplotlib.colors import LogNorm, SymLogNorm  # for log scaling of images, 
 import numpy as np
 import poppy
 import astropy.io.fits as fits
-from . utils import max_cen_phot
+from ..utils import max_cen_phot
 import matplotlib.gridspec
 from scipy import signal
 cmap=matplotlib.cm.get_cmap(name='gnuplot', lut=None)
@@ -12,6 +12,8 @@ import matplotlib.patches as patches
 import scipy.stats
 def null_diagnostic_plot(cube,background,wfs_cube=None,exp_time=1.0,name='',
                          xlo=0,xhi=-1,ylo=0,yhi=-1,
+                         radius_pixels=5,
+                         boxsize=5,
                          max_bright=1,
                          **kwargs):
     '''
@@ -47,7 +49,7 @@ def null_diagnostic_plot(cube,background,wfs_cube=None,exp_time=1.0,name='',
     #for i in range(statscube.shape[2]):
     #    plt.figure()
     #    plt.imshow(statscube[:,:,i])
-    phot=np.array([max_cen_phot(statscube[:,:,i],radius_pixels=5,fixed_center=None,verbose=True,boxsize=5)
+    phot=np.array([max_cen_phot(statscube[:,:,i],radius_pixels=radius_pixels,fixed_center=None,verbose=True,boxsize=boxsize,**kwargs)
                    for i in range(statscube.shape[2])])
     ax1.plot(x,max_vs_time/max_bright,label='max')
     ax1.set_xticklabels([]) #remove y axis labels
